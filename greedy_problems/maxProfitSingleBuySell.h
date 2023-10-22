dd08cc57ac5727c87e0f2dfcea8635c495f8255e
alvtikhonov <alvtikhonov@gmail.com>
2023-10-23 02:32:59 +0300

    [greedy_problems] Finding the maximum profit that can be achieved by choosing a single day to buy

---
+++ greedy_problems/maxProfitSingleBuySell.h
@@ -0,0 +1,44 @@
+// Best Time to Buy and Sell Stock (choosing a single day to buy)
+
+/*
+
+You are given an array prices where prices[i] is the price of a given stock on the i-th day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different
+day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit,
+return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+*/
+
+#include <vector>
+#include <algorithm>
+
+int maxProfitSingleBuySell(std::vector<int>& prices)
+{
+	int maxpro=0;
+	int minprice=prices[0];
+	
+	for(int i=0; i<prices.size(); i++)
+	{
+		minprice = std::min(minprice, prices[i]);
+		maxpro   = std::max(maxpro, prices[i]-minprice);
+	}
+	
+	return maxpro;
+}
