e0d6f445d06d8ac90adf65c9194807b2c27b855e
alvtikhonov <alvtikhonov@gmail.com>
2023-10-23 03:00:01 +0300

    [greedy_problems] Small refactoring maxProfitWithCooldown.h

--- greedy_problems/maxProfitWithCooldown.h
+++ greedy_problems/maxProfitWithCooldown.h
@@ -1,7 +1,7 @@
 // Best Time to Buy and Sell Stock with Cooldown
 
 /*
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You are given an array prices where prices[i] is the price of a given stock on the i-th day.
 
 Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e.,
 buy one and sell one share of the stock multiple times) with the following restrictions:
@@ -28,25 +28,25 @@ Output: 0
 
 int maxProfitWithCooldown(std::vector<int>& prices)
 {
-	int restAfteBuy = -prices[0], acumProfit = 0, f3 = 0;
+	int restAfteBuy = -prices[0], acumProfit = 0;
+	int delayAcumProfit = 0;
 	
 	for (int i = 1; i < prices.size(); ++i)
 	{
 		int prevRest = restAfteBuy;
-		int prevAcumProfit = acumProfit, pf3 = f3;
+		int prevAcumProfit = acumProfit;
 		
 		// Point of purchase. The remaining money after a new purchase,
 		// the lower the price - the larger the balance
-		restAfteBuy = std::max(prevRest, pf3 - prices[i]);
+		restAfteBuy = std::max(prevRest, delayAcumProfit - prices[i]);
 		
 		// Point of sale
 		acumProfit = std::max(prevAcumProfit, prevRest + prices[i]); // positive accumulated income
 		
 		// Since you cannot immediately buy after the sale, the accumulated income for calculations
 		// must be taken from the previous
-		f3 = std::max(pf3, prevAcumProfit); // maximum between
+		delayAcumProfit = std::max(delayAcumProfit, prevAcumProfit); // maximum between
 	}
 	
 	return acumProfit;
 }
-
