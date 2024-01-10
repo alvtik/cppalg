9fc8e598297d9cf6c1d2b73630525e23381dca6b
alvtikhonov <alvtikhonov@gmail.com>
2024-01-10 13:51:39 +0300

    [combinatorics] Ones ("1") combinations: added version that uses sorting

--- combinatorics/combin_fixed_count_bin_ones.h
+++ combinatorics/combin_fixed_count_bin_ones.h
@@ -3,7 +3,7 @@
 #include <vector>
 #include <stack>
 
-class OnesCombinations {
+class OnesCombStackVer {
 public:
 	std::vector<std::string> sequences(int len, int numOnes) {
 		std::vector<std::string> res;
@@ -41,9 +41,53 @@ public:
 	}
 };
 
+class OnesCombSortVers {
+public:
+	std::vector<std::string> sequences(int len, int numOnes)
+	{
+		std::vector<std::string> res;
+
+		if (len < 1 || len < numOnes || numOnes < 1)
+			return res;
+
+		std::string str;
+		std::string strEnd;
+
+		str.reserve(len);
+		strEnd.reserve(len);
+
+		strEnd.insert(strEnd.end(), len - numOnes, '0');
+		strEnd.insert(strEnd.end(), numOnes, '1');
+
+		std::size_t nLastOnePos = numOnes - 1;
+
+		do
+		{
+			if (str.size() == 0)
+			{
+				str.insert(str.end(), numOnes, '1');
+			}
+			else
+			{
+				auto nFindPos = str.find("10");
+				str[nFindPos] = '0';
+				str[nFindPos + 1] = '1';
+
+				std::sort(str.begin(), str.begin() + nFindPos, std::greater<char>());
+			}
+
+			str.insert(str.end(), len - str.size(), '0');
+			res.push_back(str);
+
+		} while (str != strEnd);
+
+		return res;
+	}
+};
+
 int main(int argc, char *argv[])
 {
-	OnesCombinations comb;
+	OnesCombStackVer comb;
 	auto res = comb.sequences(7, 4);
 
 	for (const auto& s : res)
