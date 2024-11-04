4865d2374915b7471596895c8eeb3a49b2c36df9
alvtikhonov <alvtikhonov@gmail.com>
2024-11-04 23:32:18 +0300

    [leetcode] task 5 - longest-palindromic-substring

--- leetcode/0005.cpp
+++ leetcode/0005.cpp
@@ -1,3 +1,4 @@
+//Longest Palindromic Substring
 class Solution {
 public:
     string longestPalindrome(string s) {
@@ -19,3 +20,28 @@ public:
         }
     }
 };
+
+/*
+ * Another way
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.size();
+        vector<vector<bool>> f(n, vector<bool>(n, true));
+        int k = 0, mx = 1;
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                f[i][j] = false;
+                if (s[i] == s[j]) {
+                    f[i][j] = f[i + 1][j - 1];
+                    if (f[i][j] && mx < j - i + 1) {
+                        mx = j - i + 1;
+                        k = i;
+                    }
+                }
+            }
+        }
+        return s.substr(k, mx);
+    }
+};
+ */
